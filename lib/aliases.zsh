4231e8fba41992b19ea30112541855b6e0f0003c
Devin Christensen <quixoten@gmail.com>
2013-01-25 21:08:31 -0700

    use my preferred alias for la

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -19,7 +19,7 @@ alias history='fc -l 1'
 alias lsa='ls -lah'
 alias l='ls -lA1'
 alias ll='ls -l'
-alias la='ls -lA'
+alias la='ls -A'
 alias sl=ls # often screw this up
 
 alias afind='ack-grep -il'
